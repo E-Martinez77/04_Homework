b96c70b1cdbbfc62581d465ff072265d1eefbe0f
E-Martinez77 <emartizzle2185@me.com>
2021-02-04 22:27:15 -0700

    added event listener

--- script.js
+++ script.js
@@ -1,107 +1,69 @@
-// 5 questions
+let timer;
 
-// render/unrender
+let userScore;
 
-// Container with one section min
+let initialTimer = 75; //set interval function later on.
 
-// Carousel style bootstrap layout 
+let finishers = [];
 
-// View is staying static
-
-// Question/answer set up as an array of objects
-
-// One function referencing array 
-
-//Timer space within HTML, countdown lives in Javascript *subtraction.
-
-//Quicksand_
-
-//Local storage for high score and initials
-
-//Seperate file that included questions/object
-
-//For seperate files, will a defined variable in one js reference a second one? One single.
-
-// variable= array of objects
-
-//Step 1: Write out questions
-
-//Steph 2: Build Object Arrays for questions
-
-
-//Build Question Object Array
-
-var timer 
-
-var userScore
-
-var initialTimer = 75; //set interval function later on.
-
-var finishers = []
-
-
-
-var questions = 
-[
-    {
-        question: "Commonly used data types DO NOT include:",
-        choiceA: "Strings",
-        choiceB: "Booleans",
-        choiceC: "Protractor",
-        choiceD: "Numbers",
-        correct: "Protractor",
-    },
-    {
-        question: "The condition in an if/else statement is enclosed within _______.",
-        choiceA: "Quotes",
-        choiceB: "Curly brackets",
-        choiceC: "Pita",
-        choiceD: "Square brackets",
-        correct: "Pita",
-    },
-    {
-        question: "Arrays in JavaScript can be used to store _________.",
-        choiceA: "Numbers and strings",
-        choiceB: "Other arrays", 
-        choiceC: "Booleans",
-        choiceD: "All of the above",
-        correct: "All of the above",
-    },
-    {
-        question: "String values must be enclosed within _____ when being assigned to variables.",
-        choiceA: "Commas",
-        choiceB: "Curly Brackets",
-        choiceC: "Quotes",
-        choiceD: "Hashbrowns",
-        correct: "Hashbrowns",        
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choiceA: "JavaScript",
-        choiceB: "Terminal/bash",
-        choiceC: "For loops",
-        choiceD: "console.log",
-        correct: "console.log",
-    },
-
+let questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choiceA: "Strings",
+    choiceB: "Booleans",
+    choiceC: "Protractor",
+    choiceD: "Numbers",
+    correct: "Protractor",
+  },
+  {
+    question:
+      "The condition in an if/else statement is enclosed within _______.",
+    choiceA: "Quotes",
+    choiceB: "Curly brackets",
+    choiceC: "Pita",
+    choiceD: "Square brackets",
+    correct: "Pita",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _________.",
+    choiceA: "Numbers and strings",
+    choiceB: "Other arrays",
+    choiceC: "Booleans",
+    choiceD: "All of the above",
+    correct: "All of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within _____ when being assigned to variables.",
+    choiceA: "Commas",
+    choiceB: "Curly Brackets",
+    choiceC: "Quotes",
+    choiceD: "Hashbrowns",
+    correct: "Hashbrowns",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choiceA: "JavaScript",
+    choiceB: "Terminal/bash",
+    choiceC: "For loops",
+    choiceD: "console.log",
+    correct: "console.log",
+  },
 ];
 
-// Functions dependent on how I want to write code; as little as two steps within function to complete task *focus*
+let startButton = document.getElementById("startButton");
 
-// function to start the quiz
+startButton.addEventListener("click", function () {
+  alert();
+});
 
-// click going to hide initial html, populating the questions, start timer click call function to do specific things; functions can be used for each individual task
-
-// writing individual functions for the tasks and calling multiple functions within event listeners to test individual actions
-
-// Comment out stuff as I go to test individual 
-
-// Regardless of what I have always turn it in. 
-
-// Conditional tree to determine right/wrong answer versus user selected(within function)
+// document.addEventListener("click", function () {
+//   alert;
+// });
 
 //8, 4 & 5, 20-23, 26, 27 & 28, STUDY THESE!!!!
 
-//Biggest benefit, nail down the concepts we covered today. Look into local storage and understand how to get and save items from local storage and how to add the items that we retrieve to the page.
-
-//more than 5 seconds, google it. 
+/* References: 
+https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+*/
