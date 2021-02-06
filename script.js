29fbac5e57989ca4491c3311978b9086bbf79c01
E-Martinez77 <emartizzle2185@me.com>
2021-02-05 20:03:01 -0700

    Updated formulas

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let timer;
 
-let userScore;
-
-let initialTimer = 75; //set interval function later on.
+let userScore = 0; //display to page
+let initialTimer = 75; //set interval function later on. Display
 
 let finishers = [];
 
@@ -55,32 +54,84 @@ let questions = [
 let startButton = document.getElementById("startButton");
 let startDisplay = document.getElementById("startDisplay");
 let questionDisplay = document.getElementById("questionDisplay");
+let buttons = document.querySelectorAll(".buttons");
+let timerSpan = document.getElementById("timerDisplay");
 let question = document.getElementById("question");
 let answer1 = document.getElementById("answer1");
 let answer2 = document.getElementById("answer2");
 let answer3 = document.getElementById("answer3");
 let answer4 = document.getElementById("answer4");
+let cycleIndex = 0;
 
 startButton.addEventListener("click", function () {
-  alert();
+  // alert();
   startButton.setAttribute("class", "hide");
   startDisplay.setAttribute("class", "hide");
   questionDisplay.classList.remove("hide");
+  timer = setInterval(timerFunction, 1000);
+  timerSpan.textContent = initialTimer;
 
   displayArray();
 });
 
 function displayArray() {
-  for (let i = 0; i < questions.length; i++) {
-    question.textContent = questions[i].question;
-    answer1.textContent = questions[i].choiceA;
-    answer2.textContent = questions[i].choiceB;
-    answer3.textContent = questions[i].choiceC;
-    answer4.textContent = questions[i].choiceD;
+  //probably need a for loop
+  question.textContent = questions[cycleIndex].question;
+  answer1.textContent = questions[cycleIndex].choiceA;
+  answer2.textContent = questions[cycleIndex].choiceB;
+  answer3.textContent = questions[cycleIndex].choiceC;
+  answer4.textContent = questions[cycleIndex].choiceD;
 
-    console.log(questions[i]);
+  //There needs to be something here to validate if a correct answer was clicked
+}
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (event.target.textContent === questions[cycleIndex].correct) {
+      correctAnswer();
+      userScore++;
+      console.log("User Score: " + userScore);
+    } else incorrectAnswer();
+  });
+});
+
+function correctAnswer() {
+  console.log("Correct");
+  cycleIndex++;
+  if (cycleIndex <= questions.length) {
+    displayArray();
+  } else {
+    //Set score to local storage to pull on results page
+    endGame();
   }
 }
+
+function incorrectAnswer() {
+  cycleIndex++;
+  initialTimer -= 10;
+  displayArray();
+}
+
+function timerFunction() {
+  //decrease time by 1
+  initialTimer--;
+  timerSpan.textContent = initialTimer;
+  if (initialTimer <= 0) {
+    //save score to local storage function to save to local storage DRY
+    //jump results page
+    endGame();
+  }
+}
+
+function endGame() {
+  //call whenever game ends and redirect
+  clearInterval(timer);
+}
+
+//Clear Interval
+// I am trying to cycle through each index, one at a time
+// Once an index has displayed, then
 //8, 4 & 5, 20-23, 26, 27 & 28, STUDY THESE!!!!
 
 /* References: 
